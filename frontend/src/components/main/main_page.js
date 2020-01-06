f8d85bc3769ccf18257ec11fc32ee7b3e142da2c
corina <corina.schambacher@gmail.com>
2020-01-06 13:36:27 -0800

    add navbar in app

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -5,6 +5,7 @@ class MainPage extends React.Component {
     render() {
         return (
             <div>
+                
                 <h1>partyBnB</h1>
                 <footer>
                     Copyright &copy; 2020 PNC
