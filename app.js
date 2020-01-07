451ecca1ede1c9c026c5750220c7520803074333
Nick S <nickjsaulsberry@outlook.com>
2020-01-07 01:51:29 -0600

    bug fix and imported route

--- app.js
+++ app.js
@@ -31,6 +31,6 @@ app.use(passport.initialize());
 require('./config/passport')(passport);
 
 app.use("/api/users", users);
-app.use("api/spots", spots);
+app.use("/api/spots", spots);
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
