a8469d86c0b071a6af9846d6f65fd60e7fae015b
Nick S <nickjsaulsberry@outlook.com>
2020-01-08 18:41:56 -0600

    heroku deploy

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ const spots = require("./routes/api/spots");
 const bodyParser = require('body-parser');
 const User = require('./models/User');
 const passport = require('passport');
+const path = require("path");
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
 
 
 mongoose
--- frontend/src/components/dashboard/dashboard.jsx
+++ frontend/src/components/dashboard/dashboard.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import Thumbnail from '../thumbnail/thumbnail'
-
+import { fetchSpots } from '../../util/spot_util';
 export default class Dashboard extends React.Component {
+    componentDidMount(){
+        fetchSpots().then(spots => this.setState({spots}));
+    }
+    constructor(props){
+        super(props);
+        this.state = {
+            spots: {}
+        }
+    }
     render() {
         return (
-            <div>
-                Hello Dashboard
-                <Thumbnail/>
-            </div>
-        )
+          <div>
+            {this.state.spots.map(spot => (
+              <Thumbnail 
+              title={spot.title}
+              price={spot.price.baseprice}
+              />
+            ))}
+          </div>
+        );
     }
 }
