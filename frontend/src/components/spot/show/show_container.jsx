b4d4bf23d761d2b8a57da76fcd6139cf8d07e3b0
corina <corina.schambacher@gmail.com>
2020-01-10 08:13:54 -0800

    show styling

---
+++ frontend/src/components/spot/show/show_container.jsx
@@ -0,0 +1,15 @@
+
+import { connect } from 'react-redux';
+import Show from './show';
+import {selectSpot} from '../../../reducers/selectors';
+
+
+const mapStateToProps = (state, ownProps) => ({
+ 
+});
+
+const mapDispatchToProps = dispatch => ({
+
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Show);
--- frontend/src/components/thumbnail/thumbnail.jsx
+++ frontend/src/components/thumbnail/thumbnail.jsx
@@ -11,9 +11,10 @@ class Thumbnail extends React.Component {
     }
 
     render(){
+      // console.log(this.props.id);
         return (
           <div className="thumbnail" onClick={() => this.props.history.push(`/spot/show/${this.props.id}`)}>
-            <img className="thumbnail-image" src={this.props.imageUrl} />
+            <img className="thumbnail-image" src={this.props.imageUrl} alt=""/>
 
             <div className="name">
               {this.props.title}
