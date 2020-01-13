947d3460f62a4221380a7a78cb16f2eb09d3bcc0
Nick S <nickjsaulsberry@outlook.com>
2020-01-13 15:34:10 -0600

    add fetch to bookings

--- frontend/src/components/booking/booking_show/booking_show.jsx
+++ frontend/src/components/booking/booking_show/booking_show.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
-
 export default class BookingShow extends Component {
+    componentDidMount(){
+        
+    }
     render() {
         const { booking } = this.props
         return (
