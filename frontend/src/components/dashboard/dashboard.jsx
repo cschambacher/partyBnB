59edaa404666a0e3fcf9e4a2657fd00d75e3a290
Nick S <nickjsaulsberry@outlook.com>
2020-01-17 19:12:09 -0600

    complete first suggestion

--- frontend/src/components/dashboard/dashboard.jsx
+++ frontend/src/components/dashboard/dashboard.jsx
@@ -5,12 +5,17 @@ import { Link, withRouter } from 'react-router-dom';
 import './dashboard.scss';
 import Card from './card/card';
 import party from './party1.jpg';
-
+import city from './city.jpg';
+import popular from './popular.jpg';
+import cityImage from './roof2.jpg';
+import { locationSearch } from '../../util/spot_util';
 class Dashboard extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            spots: []
+            spots: [],
+            sanFran: [],
+            sanFranClicked: false,
         }
     }
 
@@ -19,17 +24,62 @@ class Dashboard extends React.Component {
             console.log("spots", spots);
             this.setState({spots: spots.data});
         });
+         locationSearch("37.7749295", "-122.4194155").then(sanFranSpots => {
+             this.setState({sanFran: sanFranSpots.data})
+         })
     }
     render() {
-        if (this.state.spots.length > 0){
+        const sanFranDisplay = this.state.sanFranClicked ? (this.state.sanFran.map(spot => (
+            <div className="thumb-display">
+                <Thumbnail
+                    title={spot.title}
+                    description={spot.description.description}
+                    price={spot.price.basePrice.toString()}
+                    key={spot.id}
+                    imageUrl={spot.imageUrl}
+                    id={spot._id}
+                    state={spot.location.state}
+                    city={spot.location.city}
+                />
+            </div>
+        ))) : (this.state.sanFran.slice(0, 5).map(spot => (
+            
+                
+                    <Thumbnail
+                        title={spot.title}
+                        description={spot.description.description}
+                        price={spot.price.basePrice.toString()}
+                        key={spot.id}
+                        imageUrl={spot.imageUrl}
+                        id={spot._id}
+                        state={spot.location.state}
+                        city={spot.location.city}
+                    />
+            
+        )));
+        if (this.state.sanFran.length > 0){
             return (
                 <div>
                     <h1 className="explore-header">Explore PartyBnB</h1>
                     <div className="dashboard-categories">
-                        <Card imageUrl={party} title="City"/>
-                        <Card imageUrl={party} title="Popular"/>
+                        <Card imageUrl={city} title="City"/>
+                        <Card imageUrl={popular} title="Popular"/>
                         <Card imageUrl={party} title="Reviews"/>
                     </div>
+                    <h1 className="explore-header">Party BnB Cities</h1>
+                    <p className="dashboard-info">A great selection of cities for great parties</p>
+                    <div className="city-banner">
+                        <p className="banner-text-live">Live Free and</p>
+                        <p className="banner-text">Experience the Party</p>
+                    </div>
+                    <h1 className="explore-header">Places to Party near San Francisco</h1>
+                    
+                        <div className="thumb-display">
+                        {sanFranDisplay}
+                        </div>
+                        {(this.state.sanFranClicked === false) ?
+                            <p className="display-all-button">Show all {`(${this.state.sanFran.length})`}</p>
+                        : <div></div> }
                     <div className="thumb-display">
                         {
                             this.state.spots.map(spot => (
